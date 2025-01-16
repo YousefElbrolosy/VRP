a50c693a22bc8de9bf9e991a8fae0892b3c5a062
YousefElbrolosy <yousefelbrolosy@gmail.com>
2025-01-16 09:42:42 +0200

    fix: refixed django webserver

--- vrp_app/myapp/views.py
+++ vrp_app/myapp/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from vrp_classical import test
+from django.shortcuts import HttpResponse
+
 # Create your views here.
 def home(request):
-    # return HttpResponse('Hello, World!')
-    test_variable = test.test_1
-    return render(request, 'home.html', {'test_variable': test_variable})
+    return HttpResponse('Hello, World!')
